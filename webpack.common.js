bdca784c81631d09b2e268c4e2ac4470b0cb0519
weijiyang <crawling_snail@163.com>
2018-11-20 17:08:11 +0800

    edit package.json

--- webpack.common.js
+++ webpack.common.js
@@ -5,7 +5,7 @@ const analyzer = require('./webpack.bundle.analyzer.js')
 const CleanWebpackPlugin = require('clean-webpack-plugin')
 const htmlWebpackPlugin = require('html-webpack-plugin')
 
-module.exports = merge(process.env.NEV == 'ANALYZER' ? analyzer : null, {
+module.exports = merge(process.env.NODE_ENV == 'analyzer' ? analyzer : null, {
     entry: {
         main: __dirname + "/app/main.js",
         main1: __dirname + "/app/main1.js",
